fd363b6711f7ee2c9368336bdfa7441e10b779e2
Yucheng2021 <89940553+Yuchengyw6@users.noreply.github.com>
2022-03-23 18:03:30 -0400

    Add Kevin's changes

--- airquality/shinyappp.R
+++ airquality/shinyappp.R
@@ -120,27 +120,41 @@ todayovernightwind = paste(p4_1[7,17],p4_1[6,17],sep=" - ")
 tomorrowmorningwind = paste(p4_2[7,2],p4_2[6,2],sep=" - ")
 tomorrowafternoonwind = paste(p4_2[7,8],p4_2[6,8],sep=" - ")
 
-# Website 4
+# Website 4 - Air Dispersion Index
+# For this website, the numbers in the first three columns of the first table after the "ADI Early" and "ADI Late" rows will be scraped
+# These consist of a number and a description
+times<-strptime(Sys.time(),"%Y-%m-%d %H:%M:%S") # Extract the system date and time
+y<-as.character(format(times,"%Y")) # Extract and store the year value
+m<-as.character(format(times,"%m")) # Extract and store the month value
+d<-as.character(format(times,"%d")) # Extract and store the day value
+h<-as.character(format(times,"%H")) # Extract and store the hour value
 
-page4<-read_html("https://forecast.weather.gov/product.php?site=NWS&product=FWF&issuedby=PBZ")
-table4<-page4 %>%
+if (h>=4 & h<12){
+  link4<-"https://forecast.weather.gov/product.php?site=NWS&product=FWF&issuedby=PBZ"
+} else {
+  link4<-"https://forecast.weather.gov/product.php?site=NWS&issuedby=PBZ&product=FWF&format=CI&version=2&glossary=0"
+}
+
+page4<-read_html(link4) # Read in correct website link
+table4<-page4 %>% # Select the node containing the data, in  this case, all of the tables on the site will be scraped at once
   html_nodes(".glossaryProduct") %>%
   html_text()
-adiearly<-str_extract(table4,'ADI\\searly.{1,}')
-adilate<-str_extract(table4,'ADI\\slate.{1,}')
-adiearlysplit<-str_extract_all(adiearly,'\\d{1,2}\\s.{1,10}')
-adiearlytoday<-trimws(adiearlysplit[[1]][1],whitespace=" ")
-aetodvalue<-str_extract(adiearlytoday,'\\d{1,2}')
-aetoddesc<-str_extract(adiearlytoday,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}'))
-adiearlytonight<-trimws(adiearlysplit[[1]][2],whitespace=" ")
-aetonvalue<-str_extract(adiearlytonight,'\\d{1,2}')
-aetondesc<-str_extract(adiearlytonight,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}'))
-adiearlytomorrow<-trimws(adiearlysplit[[1]][3],whitespace=" ")
-aetomvalue<-str_extract(adiearlytomorrow,'\\d{1,2}')
-aetomdesc<-str_extract(adiearlytomorrow,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}'))
+adiearly<-str_extract(table4,'ADI\\searly.{1,}') # Extract ADI Early row from the first table
+adilate<-str_extract(table4,'ADI\\slate.{1,}') # Extract ADI Late row from the first table
+# ADI Early
+adiearlysplit<-str_extract_all(adiearly,'\\d{1,2}\\s.{1,9}') # Split the row into each number-description pair
+adiearlytoday<-trimws(adiearlysplit[[1]][1],whitespace=" ") # First pair is the "today" column, take the pair and remove any spaces before and after the text
+aetodvalue<-str_extract(adiearlytoday,'\\d{1,2}') # Extracts just the number from the today pair
+aetoddesc<-str_extract(adiearlytoday,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}')) # Extracts the description after the today pair
+adiearlytonight<-trimws(adiearlysplit[[1]][2],whitespace=" ") # Second pair is the "tonight" column, take the pair and remove any spaces before and after the text
+aetonvalue<-str_extract(adiearlytonight,'\\d{1,2}') # Extracts just the number from the tonight pair
+aetondesc<-str_extract(adiearlytonight,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}')) # Extracts the description after the tonight pair
+adiearlytomorrow<-trimws(adiearlysplit[[1]][3],whitespace=" ") # Third pair is the "tomorrow" column, take the pair and remove any spaces before and after the text
+aetomvalue<-str_extract(adiearlytomorrow,'\\d{1,2}') # Extracts just the number from the tonight pair
+aetomdesc<-str_extract(adiearlytomorrow,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}')) # Extracts the description after the tonight pair
 
-# Late starts here
-adilatesplit<-str_extract_all(adilate,'\\d{1,2}\\s.{1,10}')
+# ADI Late
+adilatesplit<-str_extract_all(adilate,'\\d{1,2}\\s.{1,9}') # Code here is identical to the ADI Early code, process is the same as above
 adilatetoday<-trimws(adilatesplit[[1]][1],whitespace=" ")
 altodvalue<-str_extract(adilatetoday,'\\d{1,2}')
 altoddesc<-str_extract(adilatetoday,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}'))
@@ -150,6 +164,8 @@ altondesc<-str_extract(adilatetonight,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{
 adilatetomorrow<-trimws(adilatesplit[[1]][3],whitespace=" ")
 altomvalue<-str_extract(adilatetomorrow,'\\d{1,2}')
 altomdesc<-str_extract(adilatetomorrow,regex('[:alpha:]{1,}[:space:]{1}[:alpha:]{1,}|[:alpha:]{1,}'))
+
+# These if-else statements change the descriptions of the ADIs if they are "Gen Poor" or "Gen Good" to "Generally Poor" or "Generally Good"
 if(aetoddesc=="Gen Poor"){
   aetoddesc<-"Generally Poor"
 } else if (aetoddesc=="Gen Good"){
@@ -185,6 +201,7 @@ if(altomdesc=="Gen Poor"){
   altomdesc<-"Generally Good"
 }
 
+# These lines combine the number and description into what will be shown on the report
 todaymorning<-paste(aetoddesc,"-",aetodvalue)
 todayafternoon<-paste(altoddesc,"-",altodvalue)
 tonightevening<-paste(aetondesc,"-",aetonvalue)
@@ -193,108 +210,123 @@ tomorrowmorning<-paste(aetomdesc,"-",aetomvalue)
 tomorrowafternoon<-paste(altomdesc,"-",altomvalue)
 
 # Website 5
-# surface inversion report
-## scrape the data from website
+# Data is scaraped here to calculate the Inversion Strength and Inversion Depths for the day
+# Website is updated at 7AM every day
 
-times<-strptime(Sys.Date(),"%Y-%m-%d")
-y<-as.character(format(times,"%Y"))
-m<-as.character(format(times,"%m"))
-d<-as.character(format(times,"%d"))
+if (h<8){
+  yesterday<-strptime(as.Date(Sys.Date())-1,"%Y-%m-%d") # Using yesterday's date if website hasn't updated yet
+  y5<-as.character(format(yesterday,"%Y"))
+  m5<-as.character(format(yesterday,"%m"))
+  d5<-as.character(format(yesterday,"%d")) 
+  link5<-paste("http://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR=",y5,"&MONTH=",m5,"&FROM=",d5,"12&TO=",d5,"12&STNM=72520",sep="") # Place the year, month, and day values into the link to get the data for the day
+} else {
+  link5<-paste("http://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR=",y,"&MONTH=",m,"&FROM=",d,"12&TO=",d,"12&STNM=72520",sep="")
+}
 
-y<-as.character(format(strptime(Sys.Date(),"%Y-%m-%d"),"%Y"))
-m<-as.character(format(strptime(Sys.Date(),"%Y-%m-%d"),"%m"))
-d<-as.character(format(strptime(Sys.Date(),"%Y-%m-%d"),"%d"))
-link<-paste("http://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR=",y,"&MONTH=",m,"&FROM=",d,"12&TO=",d,"12&STNM=72520",sep="")
-
-page5<-read_html(link)
-table5<-page5 %>%
+page5<-read_html(link5) # Read link
+table5<-page5 %>% # Extract the node containing the data, which is the whole table in this case
   html_nodes("pre:nth-child(2)") %>%
   html_text()
 
-fiveextract<-str_extract_all(table5,'\\n.{22}')
-fivenum<-fiveextract[[1]][-c(1:4)]
-digits<-trimws(substr(fivenum,3,22),which=c("left"))
-digitssplit<-str_extract_all(digits,'.{1}\\d{1,}.{1}\\d|\\d{1,}.{1}\\d')
-pressure<-lapply(digitssplit,`[[`,1)
-height<-lapply(digitssplit,`[[`,2)
-digitssplit[[1]][3]<-""
-temperature<-lapply(digitssplit,`[[`,3)
-five<-cbind(as.numeric(pressure),as.numeric(height),as.numeric(temperature))
-five<-five[-1,]
-colnames(five)<-c("Pressure (hPa)","Height (m)","Temperature (C)")
-tempdiff<-diff(five[,3])
-surfaceinversion<-five[which(tempdiff<=0),3][1]-five[,3][1]
-inversiondepth<-five[which(tempdiff<=0),2][1]-five[,2][1]
+fiveextract<-str_extract_all(table5,'\\n.{22}') # Extracts and splits each row of data
+fivenum<-fiveextract[[1]][-c(1:4)] # Removes the first four rows
+digits<-trimws(substr(fivenum,3,22),which=c("left")) # Removes the new line characters at the start and any whitespace characters
+digitssplit<-str_extract_all(digits,'.{1}\\d{1,}.{1}\\d|\\d{1,}.{1}\\d') # Splits the numbers in each row
+pressure<-lapply(digitssplit,`[[`,1) # Label first column values as "pressure"
+height<-lapply(digitssplit,`[[`,2) # Label second column values as "height"
+digitssplit[[1]][3]<-"" # Add an empty string for the third entry in third column since it's not present
+temperature<-lapply(digitssplit,`[[`,3) # Label third column values as "temperature" 
+five<-cbind(as.numeric(pressure),as.numeric(height),as.numeric(temperature)) # Combine these variables into a new data frame
+colnames(five)<-c("Pressure (hPa)","Height (m)","Temperature (C)") # Add names to each column with their units of measurement
+five<-five[-1,] # Remove first row, since it has no temperature value
 
-sentence<-five[which(five[,2]<1000),]
-tempdiffunder1k<-diff(sentence[,3])
-e<-which(tempdiffunder1k<0)
-f<-diff(e)
-g<-which(f>1)
-upperinversion<-function(){
-  if (is.na(tempdiffunder1k[e[g[1]+1]])){
-    print("No upper inversion starting below ~1000 m is reported")
-  } else print("Yes, an upper inversion starting below ~1000 m is reported")
-}
+# Calculation for Surface Inversion Strength and Inversion Depth
+tempdiff<-diff(five[,3]) # Create a differenced list of the temperature column, each entry subtracted from the next
+surfaceinversion<-five[which(tempdiff<0),3][1]-five[,3][1] # If the temperature increases as height increases, take the peak temperature and subtract it from the surface temperature to get Surface Inversion Strength
+inversiondepth<-five[which(tempdiff<0),2][1]-five[,2][1] # Take the height of the peak temperature and subtract the surface height (359 m) to get Inversion Depth
 
-fivestrength<-function(x){
+fivestrength<-function(x){ # Create a description value for how strong the Surface Inversion Strength is based on its value
   if (x==0){
-    print("None")
+    return("None")
   } else if (x>0 & x<1){
-    print("Slight")
+    return("Slight")
   } else if (x>=1 & x<3){
-    print("Weak")
+    return("Weak")
   } else if (x>=3 & x<5){
-    print("Moderate")
-  } else print("Strong")
+    return("Moderate")
+  } else return("Strong")
 }
 
-## extract the variables for ADI
+## To Calculate Break Time
+breaktemp<-(((inversiondepth/100)+five[which(tempdiff<0),3][1])*9/5)+32 # Take this number and match it to the weather forecast. The time of day when this temperature is reached is the break time.
+
+## Calculations for Surface Inversion Breaks
 temp5 <- paste(round(surfaceinversion,1),"°C")
 depth5 <- paste((inversiondepth),"m")
 time5 <- "9am" 
-scale5 <- fivestrength(surfaceinversion)
+scale5<- fivestrength(surfaceinversion)
+
+# Determining if there are any upper inversions
+sentence<-five[which(five[,2]<1000),] # Take all temperature values below 1000 m
+tempdiffunder1k<-diff(sentence[,3]) # Make differenced list
+e<-which(tempdiffunder1k<0) # Find any differences less than 0
+f<-diff(e) # Make second differenced list
+g<-which(f>1) # Find any values greater than 1
+upperinversion<-function(){ # Function to detect any inversion that is not a surface inversion and print "Yes" or "No"
+  if (is.na(tempdiffunder1k[e[g[1]+1]])){
+    return("No upper inversion starting below ~1000 m is reported")
+  } else return("Yes, an upper inversion starting below ~1000 m is reported")
+}
+
 inversion5 <- upperinversion()
 
 # Website 6
+# Used to calculate the Inversion Strength for the next day
+# Website is updated at 7AM every day 
 
-link6<-paste("https://rucsoundings.noaa.gov/get_soundings.cgi?data_source=GFS&start_year=",y,"&start_month_name=",month.abb[as.numeric(m)],"&start_mday=",as.numeric(d)+1,"&start_hour=12&start_min=0&n_hrs=1&fcst_len=shortest&airport=PIT&text=Ascii%20text%20%28GSL%20format%29&hydrometeors=false&startSecs=",as.numeric(as.POSIXct(Sys.Date()+1))+30000,"&endSecs=",as.numeric(as.POSIXct(Sys.Date()+1))+33600,sep="")
+if (h<8){
+  link6<-paste("https://rucsoundings.noaa.gov/get_soundings.cgi?data_source=GFS&start_year=",y,"&start_month_name=",month.abb[as.numeric(m)],"&start_mday=",as.numeric(d)+1,"&start_hour=12&start_min=0&n_hrs=1&fcst_len=shortest&airport=PIT&text=Ascii%20text%20%28GSL%20format%29&hydrometeors=false&startSecs=",as.numeric(as.POSIXct(Sys.Date()))+30000,"&endSecs=",as.numeric(as.POSIXct(Sys.Date()))+33600,sep="") # Link with the system's year, month, day, and epoch times
+} else {
+  link6<-paste("https://rucsoundings.noaa.gov/get_soundings.cgi?data_source=GFS&start_year=",y,"&start_month_name=",month.abb[as.numeric(m)],"&start_mday=",as.numeric(d)+1,"&start_hour=12&start_min=0&n_hrs=1&fcst_len=shortest&airport=PIT&text=Ascii%20text%20%28GSL%20format%29&hydrometeors=false&startSecs=",as.numeric(as.POSIXct(Sys.Date()+1))+30000,"&endSecs=",as.numeric(as.POSIXct(Sys.Date()+1))+33600,sep="")
+}
 
-page6<-read_html(link6)
-table6<-page6 %>%
+page6<-read_html(link6) # Read link
+table6<-page6 %>% # Select nodes with the needed data which is the full table
   html_nodes("p") %>%
   html_text()
 
-sixextracttoprow<-str_extract_all(table6,'9\\s{2}10000.{1,}')
-sixremainingrows<-str_extract_all(table6,'4\\s{1,}.{1,}')
-sixremainingrows<-sixremainingrows[[1]][-1]
-unsplitsix<-c(sixextracttoprow,sixremainingrows)
-splitsix<-str_extract_all(unsplitsix,'-\\d{1,}|\\d{1,}')
-type<-lapply(splitsix,`[[`,1)
-pressure<-lapply(splitsix,`[[`,2)
-height<-lapply(splitsix,`[[`,3)
-temp<-lapply(splitsix,`[[`,4)
-dewpoint<-lapply(splitsix,`[[`,5)
-winddirection<-lapply(splitsix,`[[`,6)
-windspeed<-lapply(splitsix,`[[`,7)
-six<-as.data.frame(cbind(type,as.numeric(pressure)/10,height,as.numeric(temp)/10,as.numeric(dewpoint)/10,winddirection,windspeed))
-colnames(six)<-c("Type","Pressure (mb)","Height (m)","Temperature (C)","Dew Point (C)","Wind Direction (Degrees)","Wind Speed (Knots)")
-tempdiffsix<-diff(unlist(six[,4]))[-1]
-surfaceinversion2<-six[which(tempdiffsix<=0),4][[1]]-unlist(six[,4])[1]
-inversiondepth2<-as.numeric(six[which(tempdiff<=0),3][[1]])-as.numeric(unlist(six[,3])[1])
+sixextracttoprow<-str_extract_all(table6,'9\\s{2}10000.{1,}') # Extract the first row which starts with a 9
+sixremainingrows<-str_extract_all(table6,'4\\s{1,}.{1,}') # Extract the remaining rows which all start with a 4
+sixremainingrows<-sixremainingrows[[1]][-1] # Remove the first row containing new line characters
+unsplitsix<-c(sixextracttoprow,sixremainingrows) # Combine the "9" row with the "4" rows
+splitsix<-str_extract_all(unsplitsix,'-\\d{1,}|\\d{1,}') # Separate all numbers into their own entries
+type<-lapply(splitsix,`[[`,1) # Label first column as "Type"
+pressure<-lapply(splitsix,`[[`,2) # Label second column as "Pressure"
+height<-lapply(splitsix,`[[`,3) # Label third column as "Height"
+temp<-lapply(splitsix,`[[`,4) # Label fourth column as "temp"
+dewpoint<-lapply(splitsix,`[[`,5) # Label fifth column as "Dewpoint"
+winddirection<-lapply(splitsix,`[[`,6) # Label sixth column as "Wind Direction"
+windspeed<-lapply(splitsix,`[[`,7) # Label seventh column as "Wind Speed"
 
-sixout<-function(x){
+# Create data frame with all the columns combined. Some columns need their data modified by dividing by 10
+six<-as.data.frame(cbind(type,as.numeric(pressure)/10,height,as.numeric(temp)/10,as.numeric(dewpoint)/10,winddirection,windspeed)) 
+
+colnames(six)<-c("Type","Pressure (mb)","Height (m)","Temperature (C)","Dew Point (C)","Wind Direction (Degrees)","Wind Speed (Knots)") # Give each column their names and units
+tempdiffsix<-diff(unlist(six[,4])) # Create a differenced list of the temperature column, each entry subtracted from the next
+surfaceinversion2<-six[which(tempdiffsix<0),4][[1]]-unlist(six[,4])[1] # If the temperature increases as height increases, take the peak temperature and subtract it from the surface temperature to get Surface Inversion Strength
+inversiondepth2<-as.numeric(six[which(tempdiff<0),3][[1]])-as.numeric(unlist(six[,3])[1]) # Take the height of the peak temperature and subtract the surface height (359 m) to get Inversion Depth
+
+sixout<-function(x){ # Create a description value for how strong the Surface Inversion Strength is based on its value
   if (x==0){
-    print("None")
+    return("None")
   } else if (x>0 & x<1){
-    print("Slight")
+    return("Slight")
   } else if (x>=1 & x<3){
-    print("Weak")
+    return("Weak")
   } else if (x>=3 & x<5){
-    print("Moderate")
-  } else print("Strong")
+    return("Moderate")
+  } else return("Strong")
 }
-
 
 # generate current system time for report's title
 currentDate <- Sys.Date()
